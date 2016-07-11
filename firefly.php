92a26c833af7cfb77f68ddeb62987956c71a5463
David Hatton <dhatton@iparadigms.com>
2016-07-11 12:12:31 +0100

    added new file to create the assignments

---
+++ firefly.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * @package   turnitintool
+ * @copyright 2016 Turnitin
+ *
+ */
+require_once ($CFG->dirroot.'/course/moodleform_mod.php');
+require_once ($CFG->dirroot.'/mod/turnitintool/lib.php');
